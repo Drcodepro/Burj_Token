23fafcca73f3e34d2fcbb60a4202c64e89f98d55
Drcodepro <drrahangdale01@gmail.com>
2024-07-16 17:52:31 +0530

    added dome

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -4,7 +4,7 @@ import burj_logo from "../assets/Image/burj-token.webp";
 export const NavBar = ()=>{
     return(
         <nav className=" nav-container">
-            <h1 className="text-[1.5rem] font-bold flex gap-2 items-center"><img src={burj_logo} alt="token_logo" className="h-[3rem]"/>BTDT</h1>
+            <h1 className="text-[1.5rem] font-bold flex gap-2 items-center"><img src={burj_logo} alt="token_logo" className=" nav-img h-[3rem]"/>BTDT</h1>
             <ul className=" navigation-container flex space-x-10 text-[1rem]">
                 <li>Home</li>
                 <li>About</li>
